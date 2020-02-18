dce0a7da9de5fb38d6930127cd206a51bbbc8e5a
Muhammad Naqvi <naqvi.muhammad@gmail.com>
2020-02-18 04:33:36 -0600

    create grid from input

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 'use strict'
 
-(function roboHoover() {
-    // init variables
-
+(async function roboHoover() {
     // read inputs
+    const input = await window.fetch('./input.txt')
+                              .then(response => response.text());
+
+    const inputs = input.split('\n');
+    const movements = inputs.slice(-1);
+    const points = inputs.slice(0, -1)
+                             .map(line => line.split(' ').map(point => parseInt(point)));
+
+    const gridX = points[0][0];
+    const gridY = points[0][1];
+    const grid = Array(gridX).fill(null)
+                             .map(() => Array(gridY).fill(null));
+
+
+
+
+    
+
+    // create grid
 
     // pre-process grid
 
