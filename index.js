39862f5a63c539fb9a30f6cfbf6dc0f81cfdfab7
Muhammad Naqvi <naqvi.muhammad@gmail.com>
2020-02-18 05:22:37 -0600

    mark Dirt Patches

--- index.js
+++ index.js
@@ -5,11 +5,13 @@
     const input = await window.fetch('./input.txt')
                               .then(response => response.text());
 
+    // split inputs
     const inputs = input.split('\n');
     const movements = inputs.slice(-1);
     const points = inputs.slice(0, -1)
                              .map(line => line.split(' ').map(point => parseInt(point)));
 
+    // create grid
     const gridX = points[0][0];
     const gridY = points[0][1];
     const emptyGrid = Array(gridY).fill(null)
@@ -17,16 +19,30 @@
 
     const grid = emptyGrid.map((row, y) => row.map((column, x) => new Point()));
 
+    // initiate hoover
+    const hooverStart = points[1];
+    const hoover = new Hoover(hooverStart[0], hooverStart[1]);
 
+    // mark dirt patches
+    const dirtPatches = points.slice(2)
+                              .map(dirtPatchCoordinates => {
+                                  const dirtVal = 1;
+                                  const x = dirtPatchCoordinates[0];
+                                  const y = dirtPatchCoordinates[1];
 
-
-    
-
-    // create grid
-
-    // pre-process grid
+                                  if (grid[y] && grid[y][x]) grid[y][x].dirt = dirtVal;
+                                  return dirtVal;
+                              });
 
     // run through all directions and output results
+    class Hoover {
+        constructor(x, y) { 
+            this.x = x;
+            this.y = y;
+            this.dirt = 0;
+        }
+    }
+
     class Point {
         constructor() { 
             this.dirt = 0;
